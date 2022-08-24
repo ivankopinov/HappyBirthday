d4012b03c983d2262d06152c49e1b83c55f03ae4
Иван Копинов <ikopinov@gmail.com>
2022-08-24 20:28:23 +0500

    lastvers

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,73 @@
-const buttonMan = document.querySelector(".header__button-gender_man");
-const buttonWoman = document.querySelector(".header__button-gender_woman");
+const buttonMen = document.querySelector(".header__button-gender_men");
+const buttonWomen = document.querySelector(".header__button-gender_women");
 const body = document.body;
+const cardImage = document.querySelector(".card__image");
+const cardText = document.querySelector(".card__text");
+const buttonText = document.querySelector(".header__button-change_text");
+const buttonImage = document.querySelector(".header__button-change_fon");
 
 const state = {
-  gender: body.classList.contains("woman") ? "woman" : "man",
+  gender: body.classList.contains("women") ? "women" : "men",
 };
 
-const changeToMan = () => {
-  if (state.gender !== "man") {
-    body.classList.add("man");
-    body.classList.remove("woman");
-    state.gender = "man";
+const getRandomForArr = (arr) => {
+  const randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber];
+};
+
+const getData = () => fetch("db.json").then((response) => response.json());
+
+const changeDOM = () => {
+  if (state.photo.includes("black")) {
+    cardText.style.color = "white";
+  } else {
+    cardText.style.color = "";
+  }
+  cardImage.src = `img/${state.photo}`;
+  cardText.innerHTML = state.text.replaceAll("\n", "<br>");
+};
+
+const getDataToCard = () => {
+  getData().then((data) => {
+    state.text = getRandomForArr(data.text[state.gender]);
+    state.photo = getRandomForArr(data.photo[state.gender]);
+    changeDOM();
+  });
+};
+
+const changeToMen = () => {
+  if (state.gender !== "men") {
+    body.classList.add("men");
+    body.classList.remove("women");
+    state.gender = "men";
+    getDataToCard();
   }
 };
-const changeToWoman = () => {
-  if (state.gender !== "woman") {
-    body.classList.add("woman");
-    body.classList.remove("man");
-    state.gender = "woman";
+const changeToWomen = () => {
+  if (state.gender !== "women") {
+    body.classList.add("women");
+    body.classList.remove("men");
+    state.gender = "women";
+    getDataToCard();
   }
 };
-buttonMan.addEventListener("click", changeToMan);
-buttonWoman.addEventListener("click", changeToWoman);
+
+const changeText = () => {
+  getData().then((data) => {
+    state.text = getRandomForArr(data.text[state.gender]);
+    changeDOM();
+  });
+};
+
+const changeImage = () => {
+  getData().then((data) => {
+    state.photo = getRandomForArr(data.photo[state.gender]);
+    changeDOM();
+  });
+};
+
+buttonMen.addEventListener("click", changeToMen);
+buttonWomen.addEventListener("click", changeToWomen);
+buttonText.addEventListener("click", changeText);
+buttonImage.addEventListener("click", changeImage);
+getDataToCard();
